ffb9caf8e34a5a07daa845f117fee6ab2ac91cc1
longho215 <57711902+longho215@users.noreply.github.com>
2020-09-30 19:08:17 +0700

    Add files via upload

---
+++ longho.py
@@ -0,0 +1,44 @@
+# on Ubuntu, requires install: `sudo apt-get install -y python3-tk`
+import tkinter as tk
+
+
+
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master)
+        self.pack()
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label = tk.Label(text="PyMi.vn checker")
+        self.label.pack()
+
+        self.entrythingy = tk.Entry()
+        self.entrythingy.pack()
+
+        self.contents = tk.StringVar()
+        self.entrythingy["textvariable"] = self.contents
+        self.entrythingy.bind("<Key-Return>", self.check_site)
+
+        self.hi_there = tk.Button(self)
+        self.hi_there["text"] = "Check web site up/down. Enter URL:"
+        self.hi_there["command"] = self.check_site
+        self.hi_there.pack()
+
+        self.quit = tk.Button(self, text="QUIT", command=root.destroy)
+        self.quit.pack()
+
+    def check_site(self, event=None):
+        url = self.contents.get().strip() or "https://pymi.vn"
+        if not url.startswith("http"):
+            url = "http://{}".format(url)
+
+        resp = requests.head(url, timeout=3)
+        print("{} response: {}".format(url, resp.status_code))
+
+
+root = tk.Tk()
+app = Application(master=root)
+app.master.title("My checker app")
+app.master.minsize(300, 200)
+app.mainloop()
